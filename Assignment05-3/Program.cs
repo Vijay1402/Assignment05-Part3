9dcfcbd81cb7ffbf54e03c3c3761d0330097a432
Vijayaraghavan R <rvrblr@gmail.com>
2023-11-09 14:47:13 +0530

    Assignment

---
+++ Assignment05-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment05_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(c);
+                c = a + b;
+                a = b;
+                b = c;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
